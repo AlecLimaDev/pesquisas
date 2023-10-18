a5386962cfc55f66cc28c5358ddc60c0e2543f49
Alec Lima <aleclimadev@gmail.com>
2023-10-18 15:38:14 -0300

    chore: footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,15 @@
-import * as S from "./styled"
+import * as S from "./styled";
 
 const Footer = () => {
   return (
     <>
       <S.Container>
-       
+        <p>Celular: 21 99578 - 1792</p>
+        <p>Email: aleclimadev@gmail.com</p>
+        <h3>Desenvolvedor Web: Alec Lima</h3>
       </S.Container>
     </>
-  )
-}
+  );
+};
 
 export default Footer;
--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.footer`
-    height: 20vh;
-    background-color: #000000;
-    color: orange; 
-   
-`
+  height: 20vh;
+  background-color: #141e46;
+  color: white;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 1em;
+`;
